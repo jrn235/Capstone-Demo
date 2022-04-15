071614996dc6539d193b56a2eb6f5ad56e2c66e3
Matt List <fr0stn0v4@gmail.com>
2022-04-15 11:26:41 -0700

    Fixed Histogram Script

--- hist.py
+++ hist.py
@@ -1,34 +1,60 @@
-import pymongo
 import pandas as pd
+import pymongo
 import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-import time
 from constring import *
+import numpy as np
 
+client = pymongo.MongoClient(con_string)
 
-constring = con_string
+# number of observations per object
+ast = client.ztf.asteroids
+total = ast.find({}, {"observationCounts":1})
+df_obs = pd.DataFrame(total)
 
-start = time.time()
-client = pymongo.MongoClient(constring)
-
-ztf_pym = client.ztf.ztf
-
-start = time.time()
-ssnamenr_obs = ztf_pym.aggregate([
-    {"$group": {"_id": "$ssnamenr", "observations": {"$sum": 1}}}
-])
-
-df = pd.DataFrame(ssnamenr_obs).sort_values("observations")
-
-print(df)
-
-df = df[-1000:]
-
-hist = sns.barplot(x='_id', y="observations", data = df, order = df["_id"])
-hist.set_xticklabels([])
+hist = sns.histplot(x='observationCounts', data = df_obs, bins = 100, log_scale=(False, True))
+hist.set_xlabel("Number of Observations")
+hist.set_ylabel("Number of Objects")
 sns.set(rc = {"figure.figsize": (5, 11)})
 fig = hist.get_figure()
-fig.savefig("hist.png", dpi = 300)
-end = time.time()
-print(f'{end - start}')
+fig.savefig("obs_hist.png", dpi = 300)
+fig.clf()
+
+
+
+# LC rotation period solutions: lcper
+lc = client.ztf["Light Curve Objects"]
+lcrot = lc.find({}, {"lcper":1})
+df_rot = pd.DataFrame(lcrot)
+
+hist_lc = sns.histplot(x='lcper', data = df_rot, bins = 100, log_scale=(False, True))
+hist_lc.set_xlabel("Light Curve Rotation Periods")
+hist_lc.set_ylabel("Number of Objects")
+fig = hist_lc.get_figure()
+fig.savefig("lc_hist.png", dpi = 300)
+fig.clf()
+
+
+
+# LC amplitudes for each object: lcamp
+lcamp = lc.find({}, {"lcamp":1})
+df_amp = pd.DataFrame(lcamp)
+
+hist_lcamp = sns.histplot(x='lcamp', data = df_amp, bins = 100)
+hist_lcamp.set_xlabel("Light Curve Amplitude")
+hist_lcamp.set_ylabel("Number of Objects")
+fig = hist_lcamp.get_figure()
+fig.savefig("lcamp_hist.png", dpi = 300)
+fig.clf()
+
+
+
+# asteroid color distribution: grColor
+gr_color = lc.find({}, {"grColor": 1})
+df_gr = pd.DataFrame(gr_color)
+
+gr_color_hist = sns.histplot(x = "grColor", data = df_gr, bins = 200)
+gr_color_hist.set_xlabel("g-r Color")
+gr_color_hist.set_ylabel("Count")
+fig = gr_color_hist.get_figure()
+fig.savefig("grColor_hist.png", dpi = 300)
+fig.clf()
